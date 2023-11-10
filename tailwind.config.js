2032e50f800eedca49434364c3c23b18bf6e498a
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-10 13:31:52 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#13C239",
+        white: "#F5F5F5"
+      }
+    },
   },
   plugins: [],
 }
