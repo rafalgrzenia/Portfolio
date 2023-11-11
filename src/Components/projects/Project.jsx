b13cd0fb4b9dc1c5959a30d0f885db5f3dbbd2c4
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-11 16:28:26 +0100

    Add Project.jsx component

---
+++ src/Components/projects/Project.jsx
@@ -0,0 +1,24 @@
+import ProjectDescription from "./ProjectDescription";
+import SocialButtons from "./SocialButtons";
+import TechStackButton from "./TechStackButton";
+
+export default function Project({id, title, imageUrl, description, githubUrl, websiteUrl, techStack}) {
+  return (
+    <div>
+      <ProjectDescription
+        title={title}
+        description={description}
+        imageUrl={imageUrl}
+      />
+      <SocialButtons
+        githubUrl={githubUrl}
+        websiteUrl={websiteUrl}
+      />
+      <div className="grid grid-cols-3  gap-[0.6rem] flex-wrap mt-8">
+        {techStack.map((skill) => (
+          <TechStackButton key={skill} skill={skill} />
+        ))}
+      </div>
+    </div>
+  );
+}
