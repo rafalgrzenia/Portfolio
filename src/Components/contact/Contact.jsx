433e67466f4c62f5f12980b246f9284bf5368876
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-14 14:30:02 +0100

    Add handleCopy function in Contact component

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,20 +1,47 @@
+import { useEffect, useState } from "react";
+import { copyToClipBoard } from "../../functions/copyToClipBoard";
+
 export default function Contact() {
+  const [isCopied, setisCopied] = useState(false);
+
+  function handleCopyOnClick(e) {
+    copyToClipBoard(e);
+    setisCopied(true);
+    setTimeout(() => {
+      setisCopied(false);
+    }, 2000);
+  }
+
   return (
-    <section className="flex flex-col items-center gap-10 mx-8">
+    <section id="contact" className="flex flex-col items-center gap-10 mx-8">
       <h2 className="text-[1.75rem] font-medium w-[11rem]">
         Feel free to contact me!
       </h2>
       <div className="flex flex-col gap-8">
-        <button className="text-green border border-green py-4 px-10 rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
-          <a href="">Discord</a>
+        <button
+          onClick={(e) => handleCopyOnClick(e)}
+          value={"rafalgrzenia"}
+          className="text-green border border-green py-4  w-[11rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium"
+        >
+          Discord
         </button>
-        <button className="text-green border border-green py-4 px-10 rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
-          <a href="">Linkedin</a>
+        <a href="https://www.linkedin.com/in/rafalgrzenia/" target="_blank">
+          <button className="text-green border border-green py-4 w-[11rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
+            Linkedin
+          </button>
+        </a>
+        <button
+          onClick={(e) => handleCopyOnClick(e)}
+          value={"rafalgrzenia@gmail.com"}
+          className="text-green border border-green py-4 w-[11rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium"
+        >
+          Email
         </button>
-        <button className="text-green border border-green py-4 px-10 rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
-          <a href="">Email</a>
-        </button>
-        
+        {isCopied ? (
+          <div className="copy-alert">Copied to Clipboard!</div>
+        ) : (
+          <div className="copy-alert hide">Copied to Clipboard!</div>
+        )}
       </div>
     </section>
   );
