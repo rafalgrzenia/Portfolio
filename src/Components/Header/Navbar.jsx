143b2a4a3afd250c0815cccdd795e7c80f3cf5b3
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-14 14:30:32 +0100

    Add scrolltoView function in Navbar component

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import githublogo from "../../assets/images/header/github-icon.svg";
 import linkedinlogo from "../../assets/images/header/linkedin-icon.svg";
 import Hamburger from "./Hamburger";
+import { scrollToView } from "../../functions/scrollToView";
+
+
 
 export default function Navbar() {
   const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
@@ -22,25 +25,29 @@ export default function Navbar() {
         } justify-center items-center px-6 py-6 text-center absolute top-20 transition-all duration-700  bg-[rgba(0,0,0,0.8)] `}
       >
         <ul className={`flex flex-col gap-[3.75rem] `}>
-          <li className="text-2xl">
-            <a to="">Projects</a>
+          <li onClick={() => scrollToView("projects")} className="text-2xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer ">
+           Projects
           </li>
-          <li className="text-2xl">
-            <a to="">Skills</a>
+          <li onClick={() => scrollToView("skills")} className="text-2xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer">
+           Skills
           </li>
-          <li className="text-2xl">
-            <a to="">Contact</a>
+          <li onClick={() => scrollToView("contact")} className="text-2xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer">
+           Contact
           </li>
-          <img
-            className="w-[3rem] h-auto m-auto"
-            src={githublogo}
-            alt="github icon"
-          />
-          <img
-            className="w-[3rem] h-auto mt-[-1.25rem] ml-auto mr-auto"
-            src={linkedinlogo}
-            alt="linkedin icon"
-          />
+          <a href="https://github.com/rafalgrzenia">
+            <img
+              className="w-[3rem] h-auto m-auto transition-all duration-300 hover:scale-[1.2]"
+              src={githublogo}
+              alt="github icon"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/rafalgrzenia/">
+            <img
+              className="w-[3rem] h-auto mt-[-1.25rem] ml-auto mr-auto transition-all duration-300 hover:scale-[1.2]"
+              src={linkedinlogo}
+              alt="linkedin icon"
+            />
+          </a>
         </ul>
       </nav>
     </>
