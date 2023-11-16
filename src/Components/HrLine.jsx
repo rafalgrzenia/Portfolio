8db74dd9dc394e3a83566649b66b73d126f48142
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-16 14:50:31 +0100

    Update styles in HrLine component

--- src/Components/HrLine.jsx
+++ src/Components/HrLine.jsx
@@ -1,5 +1,5 @@
 export default function HrLine() {
     return (
-        <div className="border border-[#1A202C] my-[3.75rem]"></div>
+        <div className="border border-[#1A202C] my-[3.75rem] sm:my-20"></div>
     )
 }
