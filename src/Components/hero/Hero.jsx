3dea1a739c408000c6e8ddb55b6b413d35d4f5d7
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-16 14:50:02 +0100

    Add styles for medium devices in hero section

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -8,43 +8,55 @@ import { scrollToView } from "../../functions/scrollToView";
 export default function Hero() {
   return (
     <section>
-      <div className="container flex flex-col items-center gap-10">
+      <div className="container flex flex-col items-center gap-10 sm:flex-row sm:gap-0 ">
         <img
           src={portraitLarge}
           alt="Portrait"
           srcSet={`${portraitLarge} 450w, ${portraitMedium} 340w, ${portraitSmall} 250w`}
-          className="block w-full max-w-[250px]"
+          className="block w-full max-w-[250px] sm:hidden"
         />
         <div className="flex flex-col gap-2">
-          <div className="flex gap-3">
-            <h2 className="text-white text-[1.75rem] font-medium">Hi</h2>
-            <img src={handEmoji} alt="hand-emoji" />
+          <div>
+            <div className="flex gap-3">
+              <h2 className="text-white text-[1.75rem] font-medium">Hi</h2>
+              <img src={handEmoji} alt="hand-emoji" />
+            </div>
+            <div className="flex gap-3">
+              <h2 className="text-white text-[1.75rem] font-medium">
+                I'm Rafal
+              </h2>
+              <img src={codingEmoji} alt="coding emoji" />
+            </div>
+            <h2 className="text-green text-xl font-medium sm:mt-2">
+              Front-End Developer
+            </h2>{" "}
+            <p className="w-[90%] sm:w-full mt-4">
+              I design and develop responsive, optimized and efficient websites.
+              Currently available for work opportunities.
+            </p>
           </div>
-          <div className="flex gap-3">
-            <h2 className="text-white text-[1.75rem] font-medium">I'm Rafal</h2>
-            <img src={codingEmoji} alt="coding emoji" />
-          </div>
-          <h2 className="text-green text-xl font-medium">
-            Front-End Developer
-          </h2>{" "}
-          <p className="w-[90%]">
-            I design and develop responsive, optimized and efficient websites.
-            Currently available for work opportunities.
-          </p>
-        </div>
-        <div className="flex gap-6 ">
-          <button
-            onClick={() => scrollToView("contact")}
-            className="font-medium border text-lg border-green rounded-full text-green px-4 py-[0.6rem] transition-all duration-300 hover:text-black hover:bg-green hover:font-medium "
-          >
-            Contact
-          </button>
-          <a href="">
-            <button className="font-medium border text-lg border-green rounded-full text-green px-7 py-[0.6rem] transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
-              CV
+
+          <div className="flex gap-6 mt-4">
+            <button
+              onClick={() => scrollToView("contact")}
+              className="font-medium border text-lg border-green rounded-full text-green px-4 xs:px-9 py-[0.6rem] transition-all duration-300 hover:text-black hover:bg-green hover:font-medium "
+            >
+              Contact
             </button>
-          </a>
+            <a href="">
+              <button className="font-medium border text-lg border-green rounded-full text-green px-7 xs:px-9 py-[0.6rem] transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
+                CV
+              </button>
+            </a>
+          </div>
         </div>
+
+        <img
+          src={portraitLarge}
+          alt="Portrait"
+          srcSet={`${portraitLarge} 450w, ${portraitMedium} 340w, ${portraitSmall} 250w`}
+          className="hidden w-full max-w-[250px]  sm:block"
+        />
       </div>
     </section>
   );
