c92067364d6b36523ae16dae97bc87f3fd180a02
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-17 10:28:41 +0100

    Add media queries for lage screens in Hero

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -8,29 +8,29 @@ import { scrollToView } from "../../functions/scrollToView";
 export default function Hero() {
   return (
     <section>
-      <div className="container flex flex-col items-center gap-10 sm:flex-row sm:gap-0 ">
+      <div className="container flex flex-col items-center gap-10 sm:flex-row sm:gap-0 lg:justify-evenly">
         <img
           src={portraitLarge}
           alt="Portrait"
           srcSet={`${portraitLarge} 450w, ${portraitMedium} 340w, ${portraitSmall} 250w`}
           className="block w-full max-w-[250px] sm:hidden"
         />
-        <div className="flex flex-col gap-2 ml-4">
+        <div className="flex flex-col gap-2 ml-4 md:ml-0">
           <div>
             <div className="flex gap-3">
-              <h2 className="text-white text-[1.75rem] font-medium">Hi</h2>
-              <img src={handEmoji} alt="hand-emoji" />
+              <h2 className="text-white text-[1.75rem] font-medium lg:text-[2.5rem]">Hi</h2>
+              <img src={handEmoji} alt="hand-emoji" className="lg:w-[35px]" />
             </div>
             <div className="flex gap-3">
-              <h2 className="text-white text-[1.75rem] font-medium">
+              <h2 className="text-white text-[1.75rem] font-medium lg:text-[2.5rem]">
                 I'm Rafal
               </h2>
-              <img src={codingEmoji} alt="coding emoji" />
+              <img src={codingEmoji} alt="coding emoji" className="lg:w-[35px]"  />
             </div>
-            <h2 className="text-green text-xl font-medium sm:mt-2">
+            <h2 className="text-green text-xl font-medium sm:mt-2 lg:text-[2rem]">
               Front-End Developer
             </h2>{" "}
-            <p className="w-[90%]  mt-4">
+            <p className="w-[90%] lg:w-[20rem]  mt-4 lg:text-lg">
               I design and develop responsive, optimized and efficient websites.
               Currently available for work opportunities.
             </p>
@@ -44,7 +44,7 @@ export default function Hero() {
               Contact
             </button>
             <a href="">
-              <button className="font-medium border text-lg border-green rounded-full text-green px-7 xs:px-9 py-[0.6rem] transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
+              <button className="font-medium border text-lg border-green rounded-full text-green px-7 xs:px-12 py-[0.6rem]  transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
                 CV
               </button>
             </a>
