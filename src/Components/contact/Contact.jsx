9554dabd46537b8eb823f7ac1c9a5887bad31ae3
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-17 09:36:16 +0100

    Add media queries for medium devices in Contact

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -18,21 +18,21 @@ export default function Contact() {
 
   return (
     <section id="contact" >
-      <div className="container flex flex-col items-center gap-12 ">
-        <h2 className="text-[1.75rem] font-medium w-[11rem]">
+      <div className="container flex flex-col items-center gap-12 sm:gap-[6rem]  ">
+        <h2 className="text-[1.75rem] sm:text-[2rem] font-medium w-[11rem] sm:w-full sm:text-center">
           Contact me!
         </h2>
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-8 sm:flex-row sm:gap-6">
           <button
             disabled={disabled}
             onClick={(e) => handleCopyOnClick(e)}
             value={"rafalgrzenia"}
-            className="text-green border border-green py-4  w-[11rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium"
+            className="text-green border border-green py-4  w-[10rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium"
           >
             Discord
           </button>
           <a href="https://www.linkedin.com/in/rafalgrzenia/" target="_blank">
-            <button className="text-green border border-green py-4 w-[11rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
+            <button className="text-green border border-green py-4 w-[10rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium">
               Linkedin
             </button>
           </a>
@@ -40,7 +40,7 @@ export default function Contact() {
             disabled={disabled}
             onClick={(e) => handleCopyOnClick(e)}
             value={"rafalgrzenia@gmail.com"}
-            className="text-green border border-green py-4 w-[11rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium"
+            className="text-green border border-green py-4 w-[10rem] rounded-full text-lg transition-all duration-300 hover:text-black hover:bg-green hover:font-medium"
           >
             Email
           </button>
