5fc715a1d97248b5bd0905d28e84453e6acff2ac
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-22 16:06:50 +0100

    Add NavDesktop component

---
+++ src/Components/Header/NavDesktop.jsx
@@ -0,0 +1,48 @@
+import githublogo from "../../assets/images/header/github-icon.svg";
+import linkedinlogo from "../../assets/images/header/linkedin-icon.svg";
+import { scrollToView } from "../../functions/scrollToView";
+
+export default function NavDesktop() {
+  return (
+    <nav className={`hidden lg:block bg-background  pr-8`}>
+      <ul className={`flex gap-[3.75rem] items-center `}>
+        <li
+          onClick={() => scrollToView("projects")}
+          className="text-2xl  transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer  "
+        >
+          Projects
+        </li>
+        <li
+          onClick={() => scrollToView("skills")}
+          className="text-2xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer "
+        >
+          Skills
+        </li>
+        <li
+          onClick={() => scrollToView("contact")}
+          className="text-2xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer "
+        >
+          Contact
+        </li>
+        <li>
+          <a href="https://github.com/rafalgrzenia">
+            <img
+              className="w-[3rem] transition-all duration-300 hover:scale-[1.2]"
+              src={githublogo}
+              alt="github icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/rafalgrzenia/">
+            <img
+              className="w-[3rem] transition-all duration-300 hover:scale-[1.2]"
+              src={linkedinlogo}
+              alt="linkedin icon"
+            />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
