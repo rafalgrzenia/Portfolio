361d72a7aa85ff48fe48049a0417df644d8bd047
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-11-22 11:00:11 +0100

    Update TechStackButton component

--- src/Components/projects/TechStackButton.jsx
+++ src/Components/projects/TechStackButton.jsx
@@ -1,7 +1,7 @@
 export default function TechStackButton({ skill }) {
   return (
-    <button className="py-[0.3rem] px-[0.7rem] rounded-[0.625rem] text-sm bg-techstackButton sm:text-base">
+    <div className="py-[0.3rem] px-[0.7rem] rounded-[0.625rem] text-sm bg-techstackButton sm:text-base">
       {skill}
-    </button>
+    </div>
   );
 }
