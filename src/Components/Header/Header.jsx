7de0e9d5ceb9c11ad010d893dfdee70d8037a751
Rafal Grzenia <rafalgrzenia96@gmail.com>
2023-12-12 16:01:17 +0100

    Add link to Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="mb-[3.75rem] sm:mb-20 relative">
       <div className="container lg:pr-0 pt-6 flex justify-between items-center lg:max-w-[1280px] ">
-        <a href="#">
+        <a href="https://rafalgrzenia.dev">
           <h1 className="text-3xl tracking-[0.4375rem] lg:ml-10">RG DEV</h1>
         </a>
         <NavDesktop />
