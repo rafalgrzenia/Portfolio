501c8e44efa840560bb8e9682f368d21b15122d0
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-02-06 15:59:59 +0100

    Update styles in NavDesktop

--- src/Components/Header/NavDesktop.jsx
+++ src/Components/Header/NavDesktop.jsx
@@ -8,26 +8,26 @@ export default function NavDesktop() {
       <ul className={`flex gap-[3.75rem] items-center `}>
         <li
           onClick={() => scrollToView("projects")}
-          className="text-2xl  transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer  "
+          className="text-xl  transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer  "
         >
           Projects
         </li>
         <li
           onClick={() => scrollToView("skills")}
-          className="text-2xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer "
+          className="text-xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer "
         >
           Skills
         </li>
         <li
           onClick={() => scrollToView("contact")}
-          className="text-2xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer "
+          className="text-xl transition-all duration-300 hover:scale-[1.1] hover:text-green cursor-pointer "
         >
           Contact
         </li>
         <li>
           <a href="https://github.com/rafalgrzenia" target="_blank">
             <img
-              className="w-12 h-12 transition-all duration-300 hover:scale-[1.2]"
+              className="w-10 h-10 transition-all duration-300 hover:scale-[1.2]"
               src={githublogo}
               alt="github icon"
             />
@@ -36,7 +36,7 @@ export default function NavDesktop() {
         <li>
           <a href="https://www.linkedin.com/in/rafalgrzenia/" target="_blank">
             <img
-              className="w-12 h-12 transition-all duration-300 hover:scale-[1.2]"
+              className="w-10 h-10 transition-all duration-300 hover:scale-[1.2]"
               src={linkedinlogo}
               alt="linkedin icon"
             />
