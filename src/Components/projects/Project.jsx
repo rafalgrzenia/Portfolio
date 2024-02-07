4e72e39d9cf4b0c664750452a44f3b2312b69449
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-02-07 11:46:36 +0100

    Add ProjectContent component

--- src/Components/projects/Project.jsx
+++ src/Components/projects/Project.jsx
@@ -1,3 +1,4 @@
+import ProjectContent from "./ProjectContent";
 import ProjectDescription from "./ProjectDescription";
 import ProjectImage from "./ProjectImage";
 import SocialButtons from "./SocialButtons";
@@ -16,17 +17,12 @@ export default function Project({
   return (
     <div className="flex flex-col max-w-[400px] sm:max-w-[450px]">
       <ProjectImage priority={priority} imageUrl={imageUrl} title={title} />
-      <div className="project-content ml-2">
-        <ProjectDescription title={title} description={description} />
-
-        <SocialButtons githubUrl={githubUrl} websiteUrl={websiteUrl} />
-
-        <div className="flex gap-[0.6rem] sm:gap-3 flex-wrap mt-10  sm:mt-12">
-          {techStack.map((skill) => (
-            <TechStackButton key={skill} skill={skill} />
-          ))}
-        </div>
-      </div>
+      <ProjectContent
+        description={description}
+        githubUrl={githubUrl}
+        websiteUrl={websiteUrl}
+        techStack={techStack}
+      />
     </div>
   );
 }
